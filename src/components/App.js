828964c74ca1f224558f320662ecbea76511f27c
Florian Verdonck <Florian.Verdonck@wkfs-frc.local>
2016-04-28 13:05:32 +0200

    Added Tile component

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import Subtitle from './Subtitle'
 
 import Button from './Button'
 import Notification from './Notification'
+import Tile from './Tile'
 
 export default class App extends Component {
   render () {
@@ -228,7 +229,28 @@ export default class App extends Component {
             <p className="is-text-centered">For more: <a href="http://bulma.io/documentation/overview/start/" target="_blank">see the official documentation</a></p>
             </Subtitle>
           <hr/>
-
+          
+          <Title>Tiles</Title>
+          <Tile is-ancestor>
+            <Tile is-4 is-vertical is-parent>
+              <Tile is-child box>
+                <p className="title">One</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+              </Tile>
+              <Tile is-child box>
+                <p className="title">Two</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+              </Tile>
+            </Tile>
+            <Tile is-parent>
+              <Tile is-child box>
+                <p className="title">Three</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
+                <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis. Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem, et sollicitudin felis neque sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam, finibus dignissim turpis. Sed eget varius ligula, at volutpat tortor.</p>
+                <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
+              </Tile>
+            </Tile>
+          </Tile>
         </Container>
       </Section>
     )
